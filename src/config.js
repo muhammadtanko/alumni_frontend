bcd73408994d380a449fb41d297a1a1c340f1a0d
muhammad tanko <teekay014@gmail.com>
2024-10-12 16:45:41 +0100

    minor fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const configs = {
-    baseUrl: `http://localhost:3000/api/v1`,
-    // baseUrl: `https://api.fgckwali-alumni.org/api/v1`,
+    // baseUrl: `http://localhost:3000/api/v1`,
+    baseUrl: `https://api.fgckwali-alumni.org/api/v1`,
 
 }
